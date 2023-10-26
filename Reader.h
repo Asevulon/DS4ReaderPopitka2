cb7079af67ed09adfff7cc57fc222c9d775622e0
Alex-Geine <88204321+Alex-Geine@users.noreply.github.com>
2023-10-26 22:31:19 +0300

    done
    done

--- Reader.h
+++ Reader.h
@@ -23,23 +23,23 @@ public:
 	private:
 		//	0 byte
 		//Report ID
-		char RepId;
+		unsigned char RepId;
 
 		//	1 byte
 		//Left Stick axis X 
-		char LSx;
+		unsigned char LSx;
 
 		//	2 byte
 		//Left Stick axis Y 
-		char LSy;
+		unsigned char LSy;
 
 		//	3 byte
 		//Right Stick axis X 
-		char RSx;
+		unsigned char RSx;
 
 		//	4 byte
 		//Right Stick axis Y 
-		char RSy;
+		unsigned char RSy;
 
 		//	5 byte
 		//triangle btn
@@ -73,7 +73,7 @@ public:
 
 		//	7 byte
 		//Counter
-		char Counter;
+		unsigned char Counter;
 		//T-PAD click
 		bool TPadCl;
 		//PS btn
@@ -81,18 +81,63 @@ public:
 
 		//	8 byte
 		//L2 trigger
-		char L2Trigger;
+		unsigned char L2Trigger;
 
 		//	9 byte
 		//R2 trigger
-		char R2Trigger;
+		unsigned char R2Trigger;
 
 		//	10-11 byte
 		//timestamp 
-		char TimeStamp1;
-		char TimeStamp2;
+		unsigned char TimeStamp1;
+		unsigned char TimeStamp2;
 
-		//
+
+
+
+		//	30 byte
+		//наушники
+		unsigned char HeadPh;
+		string Device;
+
+		//	31-32  byte
+		string byte3132 = "unknown";
+
+		//	33	byte
+		unsigned char TPadEvent;
+
+		//	34 byte
+		unsigned char TPADcounter;
+
+		//	35 byte
+		bool FingerOneDown;
+		unsigned char FingerOneCount;
+
+		//	36-38 byte
+
+		// 1111 1111  1111     1111  1111 1111
+		unsigned char FingerOnePos1;
+		unsigned char FingerOnePos2;
+		unsigned char FingerOnePos3;
+		unsigned int half1;
+		unsigned int half2;
+		unsigned int FingerOneX;
+		unsigned int FingerOneY;
+
+		//	39 byte
+		bool FingerTwoDown;
+		unsigned char FingerTwoCount;
+
+		//	40-42 byte
+		unsigned char FingerTwoPos1;
+		unsigned char FingerTwoPos2;
+		unsigned char FingerTwoPos3;
+		unsigned int FingerTwoX;
+		unsigned int FingerTwoY;
+
+
+
+
 	public:
 		void GetData(int id, char ch) {
 			switch (id) {
@@ -126,8 +171,108 @@ public:
 					endl << "cross btn : " << (int)CrosBtn <<
 					endl << "square btn : " << (int)SqBtn << endl;
 				break;
+			case 30:
+				HeadPh = ch;
+				if (!bool(ch ^ 0x7b))
+					Device = "Head with mic";
+
+				else if (!bool(ch ^ 0x3b))
+					Device = "Head";
+
+				else if (!bool(ch ^ 0x1b))
+					Device = "None";
+
+				else if (!bool(ch ^ 0x8))
+					Device = "Bluetooth ??";
+
+				else
+					Device = "???";
+				cout << "Device: " << Device << endl;
+
+				break;
+			case 31:
+				cout << "Byte 31: " << byte3132 << endl;
+				break;
+			case 32:
+				cout << "Byte 32: " << byte3132 << endl;
+				break;
+			case 33:
+				TPadEvent = (ch & 0xf);
+				if (!bool(TPadEvent ^ 0x0))
+					cout << "TPAD event: No data for Tpad" << endl;
+				else if (!bool(TPadEvent ^ 0x01))
+					cout << "TPAD event: Set data for 2 current touches" << endl;
+				else if (!bool(TPadEvent ^ 0x02))
+					cout << "TPAD event: set data for previous touches at [44-51]" << endl;
+				break;
+			case 34:
+				TPADcounter = ch;
+				cout << "T-PAD: auto incrementing number to track last update ? : " << (int)TPADcounter << endl;
+				break;
+			case 35:
+				FingerOneDown = (bool((1 << 7) & ch));
+				FingerOneCount = ch & 0x7f;
+				cout << "FingerOneDown (TPAD): " << FingerOneDown << endl;
+				cout << "FingerOneCounter (TPAD): " << (int)FingerOneCount << endl;
+				break;
+			case 36:
+				FingerOnePos1 = ch;
+				cout << "FingerOnePos1: " << (int)FingerOnePos1 << endl;
+				break;
+			case 37:
+				FingerOnePos2 = ch;
+				cout << "FingerOnePos2: " << (int)FingerOnePos2 << endl;
+				break;
+			case 38:
+				FingerOnePos3 = ch;
+				cout << "FingerOnePos3: " << (int)FingerOnePos3 << endl;
+				half1 = FingerOnePos2 & 0xf;
+				half2 = (FingerOnePos2 & 0xf0) >> 4;
+
+				// half1(0000 xxxx) fingerOnePos1(yyyy yyyy) res: 0000 0000 0000 0000 0000 xxxx yyyy yyyy
+				FingerOneX = (half1 << 8) | (unsigned int)FingerOnePos1;
+
+				//  fingerOnePos3(yyyy yyyy) half2(0000 xxxx) res: 0000 0000 0000 0000 0000 yyyy yyyy xxxx
+				FingerOneY = ((unsigned int)FingerOnePos3 << 4) | half2;
+
+				cout << "FingerOneX: " << FingerOneX << endl;
+				cout << "FingerOneY: " << FingerOneY << endl;
+
+				break;
+			case 39:
+				FingerTwoDown = (bool((1 << 7) & ch));
+				FingerTwoCount = ch & 0x7f;
+				cout << "FingerTwoDown (TPAD): " << FingerTwoDown << endl;
+				cout << "FingerTwoCounter (TPAD): " << (int)FingerTwoCount << endl;
+				break;
+			case 40:
+				FingerTwoPos1 = ch;
+				cout << "FingerTwoPos1: " << (int)FingerTwoPos1 << endl;
+				break;
+			case 41:
+				FingerTwoPos2 = ch;
+				cout << "FingerTwoPos2: " << (int)FingerTwoPos2 << endl;
+				break;
+			case 42:
+				FingerTwoPos3 = ch;
+				cout << "FingerTwoPos3: " << (int)FingerTwoPos3 << endl;
+				half1 = FingerTwoPos2 & 0xf;
+				half2 = (FingerTwoPos2 & 0xf0) >> 4;
+
+				// half1(0000 xxxx) fingerOnePos1(yyyy yyyy) res: 0000 0000 0000 0000 0000 xxxx yyyy yyyy
+				FingerTwoX = (half1 << 8) | (unsigned int)FingerTwoPos1;
+
+				//  fingerOnePos3(yyyy yyyy) half2(0000 xxxx) res: 0000 0000 0000 0000 0000 yyyy yyyy xxxx
+				FingerTwoY = ((unsigned int)FingerTwoPos3 << 4) | half2;
+
+				cout << "FingerTwoX: " << FingerTwoX << endl;
+				cout << "FingerTwoY: " << FingerTwoY << endl;
+
+				break;
+
 			default:
-				cout << "Значение не равно 5, 6 и даже 0";
+				cout << "id:" << id << " NULL" << endl;
+				break;
 			}
 
 		}
